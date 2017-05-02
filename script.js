f17728e5b4c05a7dc743403ddd7b53399d983f5f
WellKochi <zgqqi@126.com>
2017-05-02 15:01:20 -0400

    s

--- script.js
+++ script.js
@@ -11,7 +11,26 @@ $(function () {
      $("#lev").on("fusing", function () {
         document.querySelector('a-scene').querySelector('#cursor').emit("Fuse");
     })
-     $("#lev").on("fusing", function () {
-        document.querySelector('a-scene').querySelector('#cursor').emit("click");
+     $("#lev").on("click", function () {
+        document.querySelector('a-scene').querySelector('#cursor').emit("Click");
     })
 })
+
+        function switchTracks(switched) {
+            if (switched) {
+                var scene = document.querySelector('a-scene');
+                var getT1 = scene.getElementsByClassName("train1");
+                getT1[0].setAttribute("visible", false);
+                var getT2 = scene.getElementsByClassName("train2");
+                getT2[0].setAttribute("visible", true);
+            }
+        }
+
+        function make() {
+            var scene = document.querySelector('a-scene');
+            var getLever = scene.getElementsByClassName("lever");
+            getLever[0].setAttribute("collada-model", "lever2.dae");
+
+            var switched = true;
+            switchTracks(switched);
+        }
